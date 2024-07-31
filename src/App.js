68e03864aa8bf65e9c1d18b7e53f5cc61e7a2f62
RPraveen444 <lamechpraveen@gmail.com>
2024-07-31 22:00:48 +0530

    home and project_manager page added

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import logo from './logo.svg';
+// import './styles/App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import LandingPage from './components/LandingPage';
+import LoginPage from './components/LoginPage'; 
+import ProjectManagerHomePage from './components/ProjectManagerHomePage';
+import AddTeamMembers from './components/AddTeamMembers';
+import ResetPassword from './components/ResetPassword';
+import ForgetPassword from './components/ForgetPassword';
+import AssignTask from './components/AssignTask';
+import TrackUserActivityPage from './components/TrackUserActivityPage';
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/forget-password" element={<ForgetPassword />} />
+        <Route path="/project_manager_home_page" element={<ProjectManagerHomePage />} />
+        <Route path="/reset_password" element={<ResetPassword />} />
+        <Route path="/add_team_members" element={<AddTeamMembers />} />
+        <Route path="/assign_task" element={<AssignTask />} />
+        <Route path="/track-user-activity" element={<TrackUserActivityPage />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
